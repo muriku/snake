be5d13d10e855a7c2060c4b935f4f5db736bd7f0
ilgam HomeSSS <mamedovilgam@mail.ru>
2022-02-28 22:28:43 +0300

    Рисование змеи

--- snake/snake/Point.cs
+++ snake/snake/Point.cs
@@ -23,7 +23,33 @@ namespace snake
             y = _y;
             sym = _sym;
         }
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
 
+        public void Move(int offset, Direction direction)
+        {
+            if(direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if (direction == Direction.LEFT)
+            {
+                x = x - offset;
+            }
+            else if (direction == Direction.UP)
+            {
+                y = y + offset;
+            }
+            else if (direction == Direction.DOWN)
+            {
+                y = y - offset;
+            }
+
+        }
         public void Draw() //эта функция ничего не возвращает по этому стоит void
         {
             Console.SetCursorPosition(x, y);
--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -27,7 +27,8 @@ namespace snake
 
             //отрисовка точек
             Point p = new Point(4,5,'*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
 
 
 
