930469dd9337847a20d6ac40a9c9023792c2c9d5
ilgam HomeSSS <mamedovilgam@mail.ru>
2022-02-28 23:28:56 +0300

    Snake is moving

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -29,9 +29,22 @@ namespace snake
             Point p = new Point(4,5,'*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
-
-
-
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
 
             Console.ReadLine();
         }
