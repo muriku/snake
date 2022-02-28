52c6b0eaf097654e7cdb49a40b8a348107358a83
Ilgam_CMDA <mamedovilgam@mail.ru>
2022-02-28 11:27:13 +0300

    Научились создавать класс point и метод

--- snake/snake/Point.cs
+++ snake/snake/Point.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Point
+namespace snake
 {
-	public Point()
-	{
-		public int x;
-		public int y;
-		public char sym;
-	}
+    internal class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
 }
--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -17,7 +17,8 @@ namespace snake
             p1.x = 1;
             p1.y = 3;
             p1.sym = '*';
-            Draw(p1.x, p1.y, p1.sym);
+            p1.Draw();
+            //Draw(p1.x, p1.y, p1.sym);
 
 
 
@@ -26,21 +27,28 @@ namespace snake
             char sym1 = '*';
 
             Draw(x1, y1, sym1);*/
+            
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
+            //Draw(p2.x, p2.y, p2.sym);
 
-            int x2 = 4;
+
+            /*int x2 = 4;
             int y2 = 5;
             char sym2 = '#';
 
-            Draw(x2, y2, sym2);
+            Draw(x2, y2, sym2);*/
 
             Console.ReadLine();
         }
-        static void Draw(int x, int y, char sym)
+        /*static void Draw(int x, int y, char sym)
         {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
 
+        }
+        */
     }
 }
 
