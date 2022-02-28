41e6bc234afb8b012b95f02e8f1a3b234b25db5a
Ilgam_CMDA <mamedovilgam@mail.ru>
2022-02-28 14:24:16 +0300

    домашка + 4 точки

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -21,7 +21,66 @@ namespace snake
          
             p2.Draw();
 
-            
+            Point p3 = new Point(10, 10, ':');
+
+            p3.Draw();
+
+            Point p4 = new Point(11, 11, ':');
+
+            p4.Draw();
+
+            Point p5 = new Point(12, 12, ':');
+
+            p5.Draw();
+
+            Point p6 = new Point(13, 13, ':');
+
+            p6.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1); 
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+            pList.Add(p5);
+            pList.Add(p6);
+
+
+            char sharp = '#';
+            char star = '*';
+
+            List<char> simbols = new List<char>();
+            simbols.Add(star);
+            simbols.Add(sharp);
+
+            char _char = simbols[0];
+            char _star = simbols[1];
+
+            foreach(int j in simbols)
+            {
+                Console.WriteLine(j);
+                Console.WriteLine(sharp);
+                Console.WriteLine(star);
+            }
+
+
+
+
             Console.ReadLine();
         }
 
